09284d681d105e5ea59f3a2ebbcbcd68b3af0e93
Mark Feineigle <mfeineigle@gmail.com>
2024-06-06 02:26:22 -0400

    play '10% for me' on collect

--- collectibles/bribe.gd
+++ collectibles/bribe.gd
@@ -2,5 +2,8 @@ extends Collectible
 
 
 func _on_area_entered(area: Area2D) -> void:
-	if area in get_tree().get_nodes_in_group("hunter"):
+	var groups: Array = get_tree().get_nodes_in_group("hunter")
+	groups += get_tree().get_nodes_in_group("joes")
+	if area in groups:
 		Signals.spawn_joe.emit()
+		AudioManager.play("res://assets/audio/joe_biden_10_percent_for_me.mp3")
