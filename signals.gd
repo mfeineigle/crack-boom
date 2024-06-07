adf6def07ae3deab42c3ee7a335817c19da58b13
Mark Feineigle <mfeineigle@gmail.com>
2024-06-07 01:30:31 -0400

    organize; add fbi/private_eye

--- signals.gd
+++ signals.gd
@@ -2,13 +2,23 @@ extends Node
 
 signal spawn_joe
 
-signal caught_crack
-signal missed_crack
+
+# avoidables
+signal caught_fbi
+signal missed_fbi
+signal caught_private_eye
+signal missed_private_eye
+
+# collectibles
 signal caught_bribe
 signal missed_bribe
 signal caught_gun
 signal missed_gun
 signal caught_laptop
 signal missed_laptop
+
+# crack
+signal caught_crack
+signal missed_crack
 
 signal game_over
