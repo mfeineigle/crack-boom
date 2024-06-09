aff6bad6de93df285a58f7c8c36dc3b658356bc1
Mark Feineigle <mfeineigle@gmail.com>
2024-06-08 22:42:34 -0400

    play audio/despawn joes on _ready

--- avoidables/private_eye.gd
+++ avoidables/private_eye.gd
@@ -1,1 +1,6 @@
 extends Collectible
+
+func _ready() -> void:
+	AudioManager.play("res://assets/audio/investigators_too_hot_for_pops.mp3")
+	for joe in get_tree().get_nodes_in_group("joes"):
+		joe.queue_free()
