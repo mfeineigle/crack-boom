bcda5e53284a7e9fbd57c48e99c0a3de28c6281a
Mark Feineigle <mfeineigle@gmail.com>
2024-06-09 20:39:28 -0400

    random select laptop sprite (blank/hooker)

--- collectibles/laptop.gd
+++ collectibles/laptop.gd
@@ -1,4 +1,12 @@
 extends Collectible
 
+@onready var laptop_sprite_2d: Sprite2D = $Sprites/LaptopSprite2D
+@onready var hooker_sprite_2d: Sprite2D = $Sprites/HookerSprite2D
+
 func _ready() -> void:
+	var choice:int = randi() % 2
+	if choice == 0:
+		laptop_sprite_2d.visible = true
+	elif choice == 1:
+		hooker_sprite_2d.visible = true
 	AudioManager.play("res://assets/audio/laptop_russian_disinfo.mp3")
