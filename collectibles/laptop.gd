3d40cbb3597c3f4a9e86949b6738c9a6d6fa5944
Mark Feineigle <mfeineigle@gmail.com>
2024-06-12 17:55:34 -0400

    add commie flag, trump/stormy audio

--- collectibles/laptop.gd
+++ collectibles/laptop.gd
@@ -1,12 +1,13 @@
 extends Collectible
 
-@onready var laptop_sprite_2d: Sprite2D = $Sprites/LaptopSprite2D
+@onready var commie_sprite_2d: Sprite2D = $Sprites/CommieSprite2D
 @onready var hooker_sprite_2d: Sprite2D = $Sprites/HookerSprite2D
 
 func _ready() -> void:
 	var choice:int = randi() % 2
 	if choice == 0:
-		laptop_sprite_2d.visible = true
+		commie_sprite_2d.visible = true
+		AudioManager.play("res://assets/audio/laptop_russian_disinfo.mp3")
 	elif choice == 1:
 		hooker_sprite_2d.visible = true
-	AudioManager.play("res://assets/audio/laptop_russian_disinfo.mp3")
+		AudioManager.play("res://assets/audio/laptop_trump_stormy_crime_that_atrocity.mp3")
