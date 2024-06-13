702507d3c5e03eadd872df09acf9ff20116862b1
Mark Feineigle <mfeineigle@gmail.com>
2024-06-13 19:09:32 -0400

    play gun lying around sfx on hunter catch

--- collectibles/gun.gd
+++ collectibles/gun.gd
@@ -2,3 +2,9 @@ extends Collectible
 
 func _ready() -> void:
 	AudioManager.play("res://assets/audio/lost_a_gun.mp3")
+
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area in get_tree().get_nodes_in_group("hunter"):
+		AudioManager.play("res://assets/audio/gun_lying_around.mp3")
