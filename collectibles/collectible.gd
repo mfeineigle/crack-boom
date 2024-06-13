676171e4201944ed7dfffee58fd163f6488fa783
Mark Feineigle <mfeineigle@gmail.com>
2024-06-13 17:49:10 -0400

    slight randomization of spin speed

--- collectibles/collectible.gd
+++ collectibles/collectible.gd
@@ -1,10 +1,11 @@
 class_name Collectible
 extends Area2D
 
-var speed:int = Globals.crack_speed
+@onready var spin: int = (randi() % 3) + 3
 
+var speed:int = Globals.crack_speed
 var velocity = Vector2.DOWN
 
 func _process(delta: float) -> void:
 	position += velocity * speed * delta
-	rotation += delta
+	rotation += delta * spin
