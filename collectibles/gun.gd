de4acbda5b98fba71ae60d1ad4de0b4d7a1e724b
Mark Feineigle <mfeineigle@gmail.com>
2024-06-15 14:28:03 -0400

    play gun sfx on joe collect

--- collectibles/gun.gd
+++ collectibles/gun.gd
@@ -6,5 +6,8 @@ func _ready() -> void:
 
 
 func _on_area_entered(area: Area2D) -> void:
-	if area in get_tree().get_nodes_in_group("hunter"):
+	var targets:Array = []
+	targets.append(get_tree().get_nodes_in_group("hunter"))
+	targets.append(get_tree().get_nodes_in_group("joes"))
+	if area in targets:
 		AudioManager.play("res://assets/audio/gun_lying_around.mp3")
