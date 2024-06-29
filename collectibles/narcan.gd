4f0d0409bda9d44d7a0491aeb8a4dc872bd2c0ea
Mark Feineigle <mfeineigle@gmail.com>
2024-06-29 14:31:06 -0400

    add sfx for hunter catching narcan

--- collectibles/narcan.gd
+++ collectibles/narcan.gd
@@ -1,9 +1,10 @@
 extends Collectible
 
+var inhales: Array = ["res://assets/audio/inhale_1.mp3", "res://assets/audio/inhale_2.mp3", "res://assets/audio/nar_cant.mp3"]
 
 func _on_area_entered(area: Area2D) -> void:
 	if area in get_tree().get_nodes_in_group("hunter"):
 		Globals.crack_spawn_rate *= 1.1
+		AudioManager.play(inhales.pick_random())
 	if area in get_tree().get_nodes_in_group("joes"):
 		Globals.crack_spawn_rate *= 1.1
-		#AudioManager.play("res://assets/audio/gun_lying_around.mp3")
