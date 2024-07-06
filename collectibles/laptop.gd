3072246cc0c4c8df19c34c63a5db4425a5f85c01
Mark Feineigle <mfeineigle@gmail.com>
2024-07-06 07:57:28 -0400

    finish audio; add china flag

--- collectibles/laptop.gd
+++ collectibles/laptop.gd
@@ -3,45 +3,59 @@ extends Collectible
 @onready var commie_sprite_2d: Sprite2D = $Sprites/CommieSprite2D
 @onready var hooker_sprite_2d: Sprite2D = $Sprites/HookerSprite2D
 @onready var ukraine_sprite_2d: Sprite2D = $Sprites/UkraineSprite2D
+@onready var china_sprite_2d: Sprite2D = $Sprites/ChinaSprite2D
 
-enum CHOICE {RUSSIA, HOOKER, UKRAINE}
+enum CHOICE {RUSSIA, HOOKER, UKRAINE, CHINA}
+enum SUBCHOICE {SPAWN, COLLECT}
+
 @onready var choice:int = CHOICE.values().pick_random()
 
+var audio_choice: Dictionary = {"RUSSIA": [ ["res://assets/audio/laptop_russian_disinfo.mp3", "res://assets/audio/trump_who_was_seth_rich.mp3"],
+											["res://assets/audio/joe_i_saved_democracy.mp3", "res://assets/audio/trump_hacked_elections.mp3" ],
+										  ],
+								"HOOKER": [ ["res://assets/audio/joe_really_son.mp3", "res://assets/audio/laptop_trump_stormy_crime_that_atrocity.mp3"],
+											["res://assets/audio/joe_you_paid_for_that.mp3","res://assets/audio/trump_what_a_simp.mp3" ],
+										  ],
+								"UKRAINE": [ ["res://assets/audio/joe_smartest_tycoon.mp3", "res://assets/audio/trump_what_is_burisma.mp3"],
+											 ["res://assets/audio/joe_dont_pay_to_play.mp3","res://assets/audio/trump_youre_fired_my_line.mp3" ],
+										   ],
+								"CHINA": [ ["res://assets/audio/joe_what_billion_deal.mp3", "res://assets/audio/trump_china_owns_biden.mp3"],
+										   ["res://assets/audio/joe_beijing_ally.mp3", "res://assets/audio/trump_tough_on_chinas_pocket_book.mp3"],
+										 ],
+
+}
 
 func _ready() -> void:
 	match choice:
 		CHOICE.RUSSIA:
 			commie_sprite_2d.visible = true
-			AudioManager.play("res://assets/audio/laptop_russian_disinfo.mp3")
+			AudioManager.play(audio_choice.RUSSIA[SUBCHOICE.SPAWN].pick_random())
 		CHOICE.HOOKER:
 			hooker_sprite_2d.visible = true
-			AudioManager.play("res://assets/audio/laptop_trump_stormy_crime_that_atrocity.mp3")
+			AudioManager.play(audio_choice.HOOKER[SUBCHOICE.SPAWN].pick_random())
 		CHOICE.UKRAINE:
 			ukraine_sprite_2d.visible = true
+			AudioManager.play(audio_choice.UKRAINE[SUBCHOICE.SPAWN].pick_random())
+		CHOICE.CHINA:
+			china_sprite_2d.visible = true
+			AudioManager.play(audio_choice.CHINA[SUBCHOICE.SPAWN].pick_random())
 
 
 func _on_area_entered(area: Area2D) -> void:
 	if area in get_tree().get_nodes_in_group("hunter"):
 		Globals.crack_spawn_rate *= 1.1
-		match choice:
-			CHOICE.RUSSIA:
-				pass
-				#AudioManager.play("We saved democracy!")
-			CHOICE.HOOKER:
-				AudioManager.play("res://assets/audio/trump_what_a_simp.mp3")
-				pass
-			CHOICE.UKRAINE:
-				AudioManager.play("res://assets/audio/joe_dont_pay_to_play.mp3")
+		_choose_audio()
 	if area in get_tree().get_nodes_in_group("joes"):
 		Globals.crack_spawn_rate *= 1.1
+		_choose_audio()
+
+func _choose_audio() -> void:
 		match choice:
 			CHOICE.RUSSIA:
-				pass
-				#AudioManager.play("We saved democracy!")
-				# Who was Seth Rich?
+				AudioManager.play(audio_choice.RUSSIA[SUBCHOICE.COLLECT].pick_random())
 			CHOICE.HOOKER:
-				pass
-				#AudioManager.play("You paid for that?")
-				#AudioManager.play("Really son?")
+				AudioManager.play(audio_choice.HOOKER[SUBCHOICE.COLLECT].pick_random())
 			CHOICE.UKRAINE:
-				AudioManager.play("res://assets/audio/joe_dont_pay_to_play.mp3")
+				AudioManager.play(audio_choice.UKRAINE[SUBCHOICE.COLLECT].pick_random())
+			CHOICE.CHINA:
+				AudioManager.play(audio_choice.CHINA[SUBCHOICE.COLLECT].pick_random())
